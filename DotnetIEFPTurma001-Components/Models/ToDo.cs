685ed2a31f289bacaac62bbfc6f8f09cdadf893f
Fernando José da Silva Júnior <nandosilva.junior@gmail.com>
2023-01-23 18:00:49 +0000

    Add project files.

---
+++ DotnetIEFPTurma001-Components/Models/ToDo.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetIEFPTurma001_Components.Models
+{
+    public class ToDo
+    {
+        public string? Description { get; set; }
+        public bool Complete { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime CompleteAt { get; set; }
+
+    }
+}
